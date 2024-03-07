37b0f7b4924435d92ac720aca0cc27e501871c1a
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2024-03-07 17:25:17 +0300

    test commit

--- src/main/java/ru/andrey/ServerGameManagement/ServerGameManagementApplication.java
+++ src/main/java/ru/andrey/ServerGameManagement/ServerGameManagementApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerGameManagementApplication {
-
+	//sdfsdfsd
 	public static void main(String[] args) {
 		SpringApplication.run(ServerGameManagementApplication.class, args);
 	}

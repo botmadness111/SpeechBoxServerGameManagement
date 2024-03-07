48c9f4d0e053890c9816b40f8c48992f1dc6414f
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2024-03-07 17:22:06 +0300

    Initial commit

---
+++ src/main/java/ru/andrey/ServerGameManagement/ServerGameManagementApplication.java
@@ -0,0 +1,13 @@
+package ru.andrey.ServerGameManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGameManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGameManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/andrey/ServerGameManagement/ServerGameManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andrey.ServerGameManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGameManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
